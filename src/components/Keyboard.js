ed33264b032e961c450f7ec1d2b36df77300d1b3
adnen rebai <adnen.rebai@gmail.com>
2023-05-21 00:05:42 +0200

    First commit

---
+++ src/components/Keyboard.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import data from "../data";
+const Keyboard = () => {
+  const [selectedLetter, setSelectedLetter] = useState([]);
+  const [textAreaValue, setTextAreaValue] = useState("");
+  const [textAreaValueNumber, setTextAreaValueNumber] = useState(0);
+  const [initialData, setData] = useState(data);
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    const objIndex = initialData.findIndex((obj) => obj.item == name);
+    const newState = [...initialData];
+    newState[objIndex].value = value;
+
+    setData(newState);
+  };
+
+  // change text area
+  const handlechangeTextArea = (e) => {
+    setTextAreaValue(e.target.value);
+    calcTotal();
+  };
+
+  // handle click keys
+  const handleClickKeys =
+    ({ item, value }) =>
+    (e) => {
+      setTextAreaValue(textAreaValue.concat(item));
+      setSelectedLetter([...selectedLetter, { item, value }]);
+    };
+
+  const calcTotal = () => {
+    let total = 0;
+    let newSelectedLetters = [];
+    if (textAreaValue) {
+      for (let i = 0; i < [...textAreaValue].length; i++) {
+        let letter = data.find((l) => l.item === [...textAreaValue][i]);
+        total += letter.value;
+        newSelectedLetters.push(letter);
+      }
+    }
+    setSelectedLetter(newSelectedLetters);
+    setTextAreaValueNumber(total);
+  };
+  useEffect(() => {
+    calcTotal();
+  }, [textAreaValue.length]);
+  return (
+    <div className="app_container">
+      <textarea
+        className="screen"
+        value={textAreaValue}
+        onChange={handlechangeTextArea}
+        rows={5}
+        cols={5}
+      />
+
+      <p className="screen_number"> {textAreaValueNumber}</p>
+
+      <div className="letters_container">
+        {initialData?.map(({ item, value }) => (
+          <div key={item} className="letter_container">
+            <input
+              className="letter_input"
+              value={value}
+              type="number"
+              name={item}
+              onChange={handleChange}
+            />
+            <p className="letter_input">{item}</p>
+          </div>
+        ))}
+      </div>
+      <div className="keyboard">
+        {initialData?.map(({ name, item, value }) => {
+          if (name === "space") {
+            return (
+              <div className="keyboard_row">
+                <br/>
+                <button
+                  key={"space"}
+                  className={"keyboard_key key_space"}
+                  onClick={handleClickKeys({ item, value })}
+                >
+                  {item}
+                </button>
+              </div>
+            );
+          }
+          return (
+            <button
+              key={item}
+              className={"keyboard_key"}
+              onClick={handleClickKeys({ item, value })}
+            >
+              {item}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Keyboard;
